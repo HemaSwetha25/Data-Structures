42ae1ead03c02cdad15aee584c89d7ece17d8bf8
Hema Swetha K <158761975+HemaSwetha25@users.noreply.github.com>
2024-07-26 20:20:13 +0530

    Tree-Preorder Traversal

---
+++ PreorderTree.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class Bst{
+    Node root=null;
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int val){
+            data=val;
+            left=null;
+            right=null;
+        }
+    }
+    public void Append(int data){
+        Node newNode=new Node(data);
+        
+        if(root==null){
+            root=newNode;
+        }else{
+            Node temp=root;
+            while(true){
+                if(data < temp.data)
+                {
+                    if(temp.left != null)
+                    {
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        temp.left = newNode;
+                        break;
+                    }
+                }
+                else{
+                
+                    if(temp.right != null)
+                    {
+                        temp = temp.right;
+                    }
+                    else
+                    {
+                        temp.right = newNode;
+                        break;
+                    }
+                }
+                }
+            }
+    }
+    public void display(Node temp){
+        if(temp!=null){
+            System.out.print(temp.data+" ");
+            display(temp.left);
+            display(temp.right);
+        }
+    }
+}
+public class tree{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Bst obj=new Bst();
+        int data;
+        while ((data = sc.nextInt()) != -1) {
+            obj.Append(data);
+        }
+        obj.display(obj.root);
+    }
+}
